d9af1e5f4bb6cf62a15ae9be40806d0f98a521e1
Bakhtier Rakhmatov <rakhmatov@gmail.com>
2020-11-26 13:23:40 -0500

    Added PutMapping method

--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -20,4 +20,10 @@ public class BeerServiceImpl implements BeerService {
 	public BeerDto saveBeer(BeerDto beerDto) {
 		return BeerDto.builder().id(UUID.randomUUID()).build();
 	}
+
+	@Override
+	public void updateBeer(UUID beerId, BeerDto beerDto) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class BeerController {
 	}
 
 	@GetMapping("/{beerId}")
-	public ResponseEntity<BeerDto> getBeer(@PathVariable UUID beerId) {
+	public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
 		return new ResponseEntity<BeerDto>(beerService.getBeerById(beerId), HttpStatus.OK);
 	}
 	
@@ -45,4 +46,10 @@ public class BeerController {
 //		BeerDto saveDto = beerService.saveBeer(beerDto);
 //		return new ResponseEntity<BeerDto>(saveDto, HttpStatus.OK);
 //	}
+	
+	@PutMapping("/{beerId}")
+	public ResponseEntity<HttpStatus> handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
+		beerService.updateBeer(beerId, beerDto);
+		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+	}
 }
